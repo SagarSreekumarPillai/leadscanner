f76a9afb7673631e207c01c6638953e2d9c6e506
Sagar <sresagar@gmail.com>
2025-07-08 20:04:11 +0530

    feat(theme): setup next-themes and wrap app with ThemeProvider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,14 @@
+// pages/_app.tsx
 import "@/styles/globals.css";
+import { ThemeProvider } from "next-themes";
 import type { AppProps } from "next/app";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
+
+export default MyApp;
